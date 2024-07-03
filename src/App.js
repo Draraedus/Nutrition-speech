abc4b8cdb18def29cca02c283035fc6b57d07e15
Draraedus <adriel2002santos@gmail.com>
2024-07-03 14:59:30 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+import { BiSolidLeaf } from "react-icons/bi";
+import { FaStop } from "react-icons/fa";
+import "./App.css";
 
 function App() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AppTitle">
+        <h1>Hunt</h1>
+        <h2>Funcionalidade speech-to-text</h2>
+      </div>
+      <div className="AppContent">
+        <p>Aperte o botão abaixo e fale</p>
+        {listening ? (
+          <button
+            className="AppButton PulseAnimation"
+            onClick={SpeechRecognition.stopListening}
+          >
+            {<FaStop />}
+          </button>
+        ) : (
+          <button
+            className="AppButton"
+            onClick={() => SpeechRecognition.startListening({
+              continuous: true,
+              language: "pt-br",
+            })}
+          >
+            {<BiSolidLeaf />}
+          </button>
+        )}
+      </div>
+      <p className="AppContent">{transcript}</p>
     </div>
   );
 }
